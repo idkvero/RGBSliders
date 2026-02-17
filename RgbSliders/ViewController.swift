bf0d53a086948766e816464058ca2346a58a117b
Veronika Emelyanchenko <v.emelyanchenko@gmail.com>
2026-02-17 11:19:19 +0300

    Initial Commit

---
+++ RgbSliders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RgbSliders
+//
+//  Created by veronika on 17.02.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
